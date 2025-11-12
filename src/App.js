49db945ad2e1c2fe06162b716ad7c855c21a5ac3
Chiraji Dilipun <hchiraji@gmail.com>
2025-11-12 09:03:49 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import TodoApp from "./TodoApp";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <BrowserRouter basename="/mern_todo">
       <h1>📝 MERN TODO APP</h1>
       <TodoApp />
+      </BrowserRouter>
     </div>
   );
 }
